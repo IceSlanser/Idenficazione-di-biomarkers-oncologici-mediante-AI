c89e23e29c7fb7cb8a70a8436c4d3a9cf6d4e5ba
kevin <huangkevin7986@gmail.com>
2024-11-11 20:53:38 +0100

    test files

--- test/test.py
+++ test/test.py
@@ -1,14 +1,14 @@
 import pandas as pd  # Load and manipulate cvs, also for One-Hot Encod.
 
-from src.data_handle import(
-    load_csv, extract_type, extract_site, adapt_dataframe, optimize_missing_data
+from src.handler import(
+    load_csv, extract_type, extract_site, adapt_dataframe, optimize_missing_data, reduceVT_dataframe
 )
-
 # To display all the columns and not just the preview
 pd.set_option('display.max_columns', None)
 
 # Load the input data and extract the characteristics
-df = load_csv("../data/raw/breast_raw.csv")
+df = load_csv("../data/raw/cancer.csv")
+print(df.shape)
 sample_type_df = extract_type(df)
 sample_site_df = extract_site(df)
 
@@ -16,11 +16,7 @@ sample_site_df = extract_site(df)
 df_adapted = adapt_dataframe(df, sample_type_df)
 df_adapted.to_csv("../data/processed/adapted.csv", index=False)
 
-# Check missing values for columns and rows
-# df_adapted.isnull().sum().to_csv('../data/processed/column_missing_data.csv')
-# df_adapted.isnull().sum(axis=1).to_csv('../data/processed/row_missing_data.csv')
-
 # Optimize the dataframe removing rows and columns with a lot of missing datas
-df_optimized = optimize_missing_data(df_adapted, 0.3, 0.5)
+df_optimized = optimize_missing_data(df_adapted, 0.0, 0.1)
 df_optimized.to_csv("../data/processed/optimized.csv", index=False)
 
