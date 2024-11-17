72867680d22b50720f5a1f47a6db3f30ae0dbfde
kevin <huangkevin7986@gmail.com>
2024-11-18 00:31:27 +0100

    slides undefined

--- src/handler/extract.py
+++ src/handler/extract.py
@@ -3,13 +3,14 @@ import pandas as pd
 # Extract sample types from the dataframe
 #   - dataframe: input dataframe
 #   Extract 'sample_id' and  'value' when 'name1' is equal to 'sample_type'
-#   Map type's values: 1 references to ill samples, 9 to healthy ones
+#   Map type's values: 1 references to ill samples, 0 to healthy ones, -1 not defined
 def extract_type(dataframe: pd.DataFrame) -> pd.DataFrame:
     print("Extracting type...")
     type_df = dataframe[dataframe['name1'] == 'sample_type'][['sample_id', 'value']]
 
-    # Lambda function: map 'value' into 1 or 9
-    type_df['sample_status'] = type_df['value'].apply(lambda x: 0 if x in [11, 6] else 1)
+    # Lambda function: map 'value' into 1, 0, -1
+    type_df['sample_status'] = type_df['value'].apply(lambda x: 0 if x in [11] else (1 if x in [1, 85, 86, 2] else -1))
+    type_df = type_df[type_df['sample_status'] != -1]
 
     return type_df
 
