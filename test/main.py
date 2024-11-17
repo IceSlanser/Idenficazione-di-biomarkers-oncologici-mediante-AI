f536291836d42a198fcff6bc61fbe6fdda5cbf3f
kevin <huangkevin7986@gmail.com>
2024-11-18 00:40:53 +0100

    test files

--- test/main.py
+++ test/main.py
@@ -1,40 +1,35 @@
 import pandas as pd
 
 from src.handler import (load_csv, optimize_missing_data, reduceVT_dataframe)
-from src.utils import (describe_missing, update_missing, visualize_feature)
+from src.utils import (update_missing, visualize_feature)
 from src.machine_learning import (split_data)
 from src.machine_learning.xgboost import build_XGB_model
 
 ## To display all the columns and not just the preview
 pd.set_option('display.max_columns', None)
 
+## Load adapted dataframe
 # df_adapted= load_csv("../data/processed/adapted.csv")
 # print(df_adapted.shape)
-# print((1583, 57713))
 
-df_optimized = load_csv("../data/processed/optimized.csv")
-print(df_optimized.shape)
-
+## Load reduced dataframe
 # df_reduced = reduceVT_dataframe(df_adapted, 0.01)
 # print(df_reduced.shape)
 
-# df_optimized = optimize_missing_data(df_adapted, 0.47, 0.99)
-# df_optimized= load_csv("/home/kevin/PycharmProjects/Idenficazione di biomarkers oncologici mediante AI/data/processed/adapted.csv")
+## Load optimized dataframe
+df_optimized= load_csv("../data/processed/optimized.csv")
+# print(df_optimized.shape)
 
 ## Check missing values for columns and rows
-# update_missing(df_optimized, '../data/processed/column_missing_data.csv', '../data/processed/row_missing_data.csv')
-## Print missing stats
-# describe_missing('../data/processed/row_missing_data.csv', '../data/processed/column_missing_data.csv')
-
-## Dividing X:characteristic_set and y:target_set
-# X = df_optimized.drop(columns=['sample_status']).copy()
-# y = df_optimized['sample_status'].copy()
-# y.to_csv("../data/processed/target_set.csv", index=False)
-# X.to_csv("../data/processed/characteristic_set.csv", index=False)
-# visualize_feature(X, y)
+# update_missing(df_optimized)
 
 ## Split data
 X_train, X_test, y_train, y_test = split_data(df_optimized, 'sample_status')
-print(X_train.shape)
+print(X_test.shape)
+
+## Visualize feature
+# visualize_feature(X_train, y_train)
+
+## Data training and tree building
 build_XGB_model(X_train, X_test, y_train, y_test)
 
--- test/test.py
+++ test/test.py
@@ -1,22 +1,33 @@
 import pandas as pd  # Load and manipulate cvs, also for One-Hot Encod.
 
 from src.handler import(
-    load_csv, extract_type, extract_site, adapt_dataframe, optimize_missing_data, reduceVT_dataframe
+    load_csv, extract_type, extract_site, adapt_dataframe, optimize_missing_data, classify_not_defined_with_kmeans
 )
-# To display all the columns and not just the preview
+## To display all the columns and not just the preview
 pd.set_option('display.max_columns', None)
 
-# Load the input data and extract the characteristics
+## Load the input data and extract the characteristics
 df = load_csv("../data/raw/cancer.csv")
-print(df.shape)
+print(df[(df['name1'] == 'sample_type') & (df['value'] == 6)].shape)
+print("(103006201, 5)")
 sample_type_df = extract_type(df)
 sample_site_df = extract_site(df)
+# sample_status
+# 1    3101
+# 0     949
+# Name: count, dtype: int64
 
-# Adapt the dataframe as wished: unique sample_id as rows and gene as columns merged with the type df
-df_adapted = adapt_dataframe(df, sample_type_df)
-df_adapted.to_csv("../data/processed/adapted.csv", index=False)
-
-# Optimize the dataframe removing rows and columns with a lot of missing datas
-df_optimized = optimize_missing_data(df_adapted, 0.0, 0.1)
-df_optimized.to_csv("../data/processed/optimized.csv", index=False)
+# ## Adapt the dataframe as wished: unique sample_id as rows and gene as columns merged with the type df
+# df_adapted = adapt_dataframe(df, sample_type_df)
+# df_adapted.to_csv("../data/processed/adapted.csv", index=False)
+# # df_adapted = load_csv("../data/processed/adapted.csv")
+# # print(df_adapted.shape)
+#
+# ## Optimize the dataframe removing rows and columns with a lot of missing datas
+# df_optimized = optimize_missing_data(df_adapted, 0.0, 0.1)
+# df_optimized.to_csv("../data/processed/optimized.csv", index=False)
+#
+# ## Classify not defined status (-1)
+# df_classified = classify_not_defined_with_kmeans(df_optimized)
+# df_optimized.to_csv("../data/processed/classified.csv", index=False)
 
